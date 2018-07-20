df360a2acab175c9818e54a995d92d0c766dbbe2
Radoslaw Mejer <radmen@desmart.com>
2018-07-20 14:11:19 +0200

    Add TaggedValue

---
+++ src/TaggedValue.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Baethon\Union;
+
+final class TaggedValue
+{
+    /**
+     * @var TagSignature
+     */
+    private $signature;
+
+    /**
+     * @var array
+     */
+    private $arguments;
+
+    private function __construct(TagSignature $signature, array $arguments = [])
+    {
+        $this->validateArguments($signature, $arguments);
+
+        $this->signature = $signature;
+        $this->arguments = $arguments;
+    }
+
+    public static function of(TagSignature $signature, array $arguments = []): self
+    {
+        return new static($signature, $arguments);
+    }
+
+    public function hasType(TagSignature $signature): bool
+    {
+        return $this->signature->sameType($signature);
+    }
+
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    private function validateArguments(TagSignature $signature, array $arguments)
+    {
+        $argumentsLength = count($arguments);
+        $parametersLength = count($signature->getParameters());
+
+        if ($argumentsLength !== $parametersLength) {
+            throw new \LengthException("Tag {$signature->getType()} requires {$parametersLength} arguments, {$argumentsLength} provided.");
+        }
+    }
+}
---
+++ test/TaggedValueTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Test;
+
+use Baethon\Union\TaggedValue;
+use Baethon\Union\TagSignature;
+
+class TaggedValueTest extends \PHPUnit\Framework\TestCase
+{
+    private $signature;
+
+    public function setUp()
+    {
+        $this->signature = TagSignature::fromString('Some:x');
+    }
+
+    public function test_it_can_be_created()
+    {
+        $tag = TaggedValue::of($this->signature, [1]);
+
+        $this->assertEquals([1], $tag->getArguments());
+        $this->assertTrue($tag->hasType($this->signature));
+    }
+
+    /**
+     * @dataProvider invalidArgumentsProvider
+     */
+    public function test_it_validates_length_of_arguments(TagSignature $signature, array $arguments)
+    {
+        $this->expectException(\LengthException::class);
+
+        TaggedValue::of($signature, $arguments);
+    }
+
+    public function invalidArgumentsProvider()
+    {
+        return [
+            [TagSignature::fromString('None'), [1]],
+            [TagSignature::fromString('Stark:name,latname'), ['Jon']],
+        ];
+    }
+}
